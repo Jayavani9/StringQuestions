524d5fdb1c81eef6699138b6e92ef5c1e181f3c5
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-11-05 05:38:27 -0800

    Update validAnagram.java

--- validAnagram.java
+++ validAnagram.java
@@ -14,3 +14,39 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 
+//1. Using Sorting 
+class Solution {
+    //Tc: O(nlogn)  Sc: O(1)
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        /*
+        char s1[] = s.toCharArray();
+        char s2[] = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        return Arrays.equals(s1,s2);
+        }
+}
+
+2. Using a counter Array
+class Solution {
+    // Tc: O(n)   Sc: O(1) as it O(26) n here it 26 and can be considered constant
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int cntr[] = new int[26];
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            cntr[s.charAt(i) - 'a']++;
+            cntr[t.charAt(i) - 'a']--;
+        }
+
+        for(int c:cntr){
+            if(c != 0) return false;
+        }
+        return true;
+        
+    }
+}
